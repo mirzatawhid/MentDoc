fb3e7a46f8b93238157ac672afdf11dbcd0a720b
mirzatawhid <dhakaiyacoder@gmail.com>
2023-11-27 01:10:15 +0600

    Runnable

--- app/src/main/java/com/myfirstapp/mentdoc/Login.java
+++ app/src/main/java/com/myfirstapp/mentdoc/Login.java
@@ -2,7 +2,9 @@ package com.myfirstapp.mentdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goRegister(View view) {
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
     }
 }
